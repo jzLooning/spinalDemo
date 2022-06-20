b97d96d5a63e83f1270eb357988c14044bb241fb
longjiazeng <424385538@qq.com>
2022-06-20 18:31:29 +0800

    设置top类

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,17 @@
 
 import Cache.{ICache, mmap}
 import spinal.core._
+import Pipe_line._
 
 
-class test extends Component {
-    val cache = new ICache
-    val mmap_ = new mmap
-    cache.io_mmap <> mmap_.io_inst
+class CpuTop extends Component {
+    val config = new ConfigPipeline
+    val if_stage = new IFStage(config)
+    val mmap = new PipeMmap
+    mmap.io_inst <> if_stage.io_mmap
 }
 object Main{
   def main(args: Array[String]):Unit = {
-      SpinalConfig(targetDirectory = "./build/",oneFilePerComponent = true).generateVerilog(new test)
+      SpinalConfig(targetDirectory = "./build/",oneFilePerComponent = true).generateVerilog(new CpuTop)
   }
 }
