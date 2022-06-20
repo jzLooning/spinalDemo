a75f35e6a56f5b66a54a956542e5f435f31edb50
longjiazeng <424385538@qq.com>
2022-06-20 18:29:32 +0800

    增加了一个文件，当作宏定义

---
+++ src/main/scala/Pipe_line/ConfigPipeline.scala
@@ -0,0 +1,28 @@
+package Pipe_line
+
+class ConfigPipeline {
+    /**`define BR_BUS_WD        33*/
+    val BR_BUS_WD = 33
+    /**`define FS_TO_DS_BUS_WD  64*/
+    val FS_TO_DS_BUS_WD = 64
+    /**`define DS_TO_ES_BUS_WD  166*/
+    val DS_TO_ES_BUS_WD = 166
+    /**`define ES_TO_MS_BUS_WD  88*/
+    val ES_TO_MS_BUS_WD = 88
+    /**`define MS_TO_WS_BUS_WD  78*/
+    val MS_TO_WS_BUS_WD = 78
+    /**`define WS_TO_RF_BUS_WD  38*/
+    val WS_TO_RF_BUS_WD = 38
+    /**`define DS_READY_GO_BUS  67*/
+    val DS_READY_GO_BUS = 67
+    /**`define ID_TO_BUBBLE     59*/
+    val ID_TO_BUBBLE = 59
+    /**`define EX_TO_BUBBLE     112*/
+    val EX_TO_BUBBLE = 112
+    /**`define ME_TO_BUBBLE     40*/
+    val ME_TO_BUBBLE = 40
+    /**`define WB_TO_BUBBLE     40*/
+    val WB_TO_BUBBLE = 40
+    /**`define BUBBLE_TO_WB     75*/
+    val BUBBLE_TO_WB = 75
+}
