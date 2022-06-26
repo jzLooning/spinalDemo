c21e984ea858e86f63837809f904e2cb8cac87e5
Lonjiazeng <1050706826@qq.com>
2022-06-26 19:07:04 +0800

    删掉了原来的类宏定义，不用bus了，直接把bus里的内容设置成输入输出

--- src/main/scala/Pipe_line/ConfigPipeline.scala
+++ src/main/scala/Pipe_line/ConfigPipeline.scala
@@ -1,28 +1,19 @@
 package Pipe_line
 
-class ConfigPipeline {
-    /**`define BR_BUS_WD        33*/
-    val BR_BUS_WD = 33
-    /**`define FS_TO_DS_BUS_WD  64*/
-    val FS_TO_DS_BUS_WD = 64
-    /**`define DS_TO_ES_BUS_WD  166*/
-    val DS_TO_ES_BUS_WD = 166
-    /**`define ES_TO_MS_BUS_WD  88*/
-    val ES_TO_MS_BUS_WD = 88
-    /**`define MS_TO_WS_BUS_WD  78*/
-    val MS_TO_WS_BUS_WD = 78
-    /**`define WS_TO_RF_BUS_WD  38*/
-    val WS_TO_RF_BUS_WD = 38
-    /**`define DS_READY_GO_BUS  67*/
-    val DS_READY_GO_BUS = 67
-    /**`define ID_TO_BUBBLE     59*/
-    val ID_TO_BUBBLE = 59
-    /**`define EX_TO_BUBBLE     112*/
-    val EX_TO_BUBBLE = 112
-    /**`define ME_TO_BUBBLE     40*/
-    val ME_TO_BUBBLE = 40
-    /**`define WB_TO_BUBBLE     40*/
-    val WB_TO_BUBBLE = 40
-    /**`define BUBBLE_TO_WB     75*/
-    val BUBBLE_TO_WB = 75
+import spinal.core.{SpinalEnum, SpinalEnumEncoding}
+import spinal.lib.experimental.chisel.Bundle
+
+class ConfigPipeline extends SpinalEnum{
+    /** ALU_OP */
+    val ALU_OP = new Bundle {
+        val add = 0
+        val sub = 1
+        val and = 2
+        val or = 3
+        val xor = 4
+        val sll = 5
+        val srl = 6
+        val lui = 7
+        val mul = 8
+    }
 }
