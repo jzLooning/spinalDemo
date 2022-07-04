39cd83c7fe321be4dd81613b67759a5cf914025e
Lonjiazeng <1050706826@qq.com>
2022-07-04 11:23:11 +0800

    函数没用，删掉了

--- src/main/scala/Pipe_line/ConfigPipeline.scala
+++ src/main/scala/Pipe_line/ConfigPipeline.scala
@@ -1,6 +1,6 @@
 package Pipe_line
 
-import spinal.core.{SpinalEnum, SpinalEnumEncoding}
+import spinal.core.{Bits, Bool, Bundle, SpinalEnum, SpinalEnumEncoding}
 import spinal.lib.experimental.chisel.Bundle
 
 class ConfigPipeline extends SpinalEnum{
@@ -15,5 +15,25 @@ class ConfigPipeline extends SpinalEnum{
         val srl = 6
         val lui = 7
         val mul = 8
+    }
+    def getBoolValue(dest:Bool,io:Bundle):Bool = {
+        val name = dest.getName()
+        var return_element = Bool(false)
+        for ((io_name,io_element) <- io.elements) {
+            if(name == io_name) {
+                return_element = io_element.asBits.asBool
+            }
+        }
+        return_element
+    }
+    def getBitsValue(dest:Bits,io:Bundle):Bits = {
+        val name = dest.getName()
+        var return_element = Bits()
+        for ((io_name,io_element) <- io.elements) {
+            if (name == io_name) {
+                return_element = io_element.asBits
+            }
+        }
+        return_element
     }
 }
